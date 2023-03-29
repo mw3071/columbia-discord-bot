fff83bcfff9322204d68112d6bfef49787e08b10
mw3071 <103603425+mw3071@users.noreply.github.com>
2023-03-28 21:36:39 -0400

    created the student class, and implemented commands

--- columbia_discord_bot.py
+++ columbia_discord_bot.py
@@ -1,6 +1,7 @@
 import discord
 import pandas as pd
 import json
+from student import Student
 from discord.ext import commands
 
 
@@ -9,8 +10,8 @@ with open('config.json', 'r') as cfg:
     token = data["token"]
 
 
-def create_user(username):
-    df.loc[len(df)] = [len(df)-1, username, "", [], []]
+# def create_user(username):
+    # df.loc[len(df)] = [len(df)-1, username, "", [], []]
     # print("df")
     # print(df)
 
@@ -18,14 +19,16 @@ print(token)
 CHANNEL_ID = '1079912337571598438'
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
-df = pd.DataFrame()
-df['index'] = []
-df['username'] = []
-df['uni'] = []
-df['prof'] = [[]]
-df['classes'] = [[]]
+# df = pd.DataFrame()
+# df['index'] = []
+# df['username'] = []
+# df['uni'] = []
+# df['prof'] = [[]]
+# df['classes'] = [[]]
 
 # print(df)
+users = {}
+s = None
 
 @bot.event
 async def on_ready():
@@ -37,45 +40,61 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send('pong!')
 
+@bot.command(help="creates a new student, must give it a uni")
+async def createuser(ctx):
+    username = str(ctx.author)
+    uni = ctx.message.content.split(' ')[1]
+    print(uni)
+    print(username)
+    s = Student(username, uni)
+    users[username] = s
+    print(s)
+    print(users)
+    await ctx.send('created!')
+
 @bot.command()
 async def addclass(ctx):
     username = str(ctx.author)
-    print(username)
-    class_name = ctx.message.content.split(' ')[1]
-    # print(class_name)
-    print("df username: ")
-    print(df['username'])
-    if df['username'].eq(username).any():
-        print("in df")
-    else:
-    # if username not in df['username']:
-        create_user(username)
-    # index = df.loc[df['username']==username, 'index']
-    # temp = df.iloc[index]['classes'][0]
-    # temp.append(class_name)
-    # df.iloc[index]['classes'][0] = temp
-        
+    users[username].add_class(ctx.message.content.split(' ')[1])
+    print(users[username])
+
+    # username = str(ctx.author)
+    # print(username)
+    # class_name = ctx.message.content.split(' ')[1]
+    # # print(class_name)
+    # print("df username: ")
+    # print(df['username'])
+    # if df['username'].eq(username).any():
+    #     print("in df")
+    # else:
+    # # if username not in df['username']:
+    #     create_user(username)
+    # # index = df.loc[df['username']==username, 'index']
+    # # temp = df.iloc[index]['classes'][0]
+    # # temp.append(class_name)
+    # # df.iloc[index]['classes'][0] = temp
+    
     await ctx.send('Added class!')
 
 @bot.command()
 async def addprof(ctx):
     await ctx.send('Added prof!')
 
-@bot.command()
-async def addrestaurant(ctx):
-    await ctx.send('Added rest!')
+# @bot.command()
+# async def addrestaurant(ctx):
+#     await ctx.send('Added rest!')
 
-@bot.command()
-async def getusernames(ctx):
-    await ctx.send(df['username'])
+# @bot.command()
+# async def getusernames(ctx):
+#     await ctx.send(df['username'])
 
-@bot.command()
-async def getclasses(ctx):
-    await ctx.send(df['class_name'][0])
+# @bot.command()
+# async def getclasses(ctx):
+#     await ctx.send(df['class_name'][0])
 
-@bot.command()
-async def getunis(ctx):
-    await ctx.send(df['uni'])
+# @bot.command()
+# async def getunis(ctx):
+#     await ctx.send(df['uni'])
 
 bot.run(token)
 
