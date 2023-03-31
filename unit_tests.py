ee7afc3b59e7f329411bb6998882c602b893b49b
mw3071 <103603425+mw3071@users.noreply.github.com>
2023-03-30 20:14:02 -0400

    edited tests and integrated pytest

--- unit_tests.py
+++ unit_tests.py
@@ -1,53 +1,58 @@
 from student import Student
 import pytest
 
-s = None
-
-def test_add_class(s):
+def test_add_class():
+    s = Student('test-username', 'test-uni')
     print('testing add class')
     s.add_class('test-class')
     assert s.classes == ['test-class']
     print('done')
 
-def test_add_prof(s):
+def test_add_prof():
+    s = Student('test-username', 'test-uni')
     print('testing add profs')
     s.add_prof('test-prof')
     assert s.profs == ['test-prof']
     print('done')
 
-def test_remove_class(s):
+def test_remove_class():
+    s = Student('test-username', 'test-uni')
     print('testing remove class')
+    s.add_class('test-class')
     s.remove_class('test-class')
     assert s.classes == []
     print('done')
 
-def test_remove_prof(s):
+def test_remove_prof():
+    s = Student('test-username', 'test-uni')
     print('testing remove profs')
+    s.add_prof('test-prof')
     s.remove_prof('test-prof')
     assert s.profs == []
     print('done')
 
-def test_set_uni(s):
+def test_set_uni():
+    s = Student('test-username', 'test-uni')
     print('testing set uni')
     s.set_uni('uni-test')
     assert s.uni == 'uni-test'
 
-def test_get_classes(s):
+def test_get_classes():
+    s = Student('test-username', 'test-uni')
+    s.add_class('test-class')
     assert s.get_classes() == ['test-class']
 
-def test_get_profs(s):
-    assert s.get_profs() == ['test-prof']
-
-def run_all():
-    print('running tests')
+def test_get_profs():
     s = Student('test-username', 'test-uni')
-    test_set_uni(s)
-    test_add_class(s)
-    test_add_prof(s)
-    test_get_classes(s)
-    test_get_profs(s)
-    test_remove_class(s)
-    test_remove_prof(s)
-    print('all tests complete')
-
-run_all()
+    s.add_prof('test-prof')
+    assert s.get_profs() == ['test-prof']
+# def run_all():
+#     print('running tests')
+#     s = Student('test-username', 'test-uni')
+#     test_set_uni(s)
+#     test_add_class(s)
+#     test_add_prof(s)
+#     test_get_classes(s)
+#     test_get_profs(s)
+#     test_remove_class(s)
+#     test_remove_prof(s)
