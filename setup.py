8131717ae8d0500356d854faab5f82ece9f65caa
mw3071 <103603425+mw3071@users.noreply.github.com>
2023-04-07 20:08:21 -0400

    edited setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
 from setuptools import setup
 
 
-setup()
+setup(
+    name='columbia-discord-bot',
+    version='0.1.0',
+    description="Discord bot for Columbia.",
+    author='Matthew Wang',
+    author_email='mw3071@columbia.edu',
+    maintainer='mw3071',
+    maintainer_email='mw3071@columbia.edu',
+    url='https://github.com/mw3071/columbia-discord-bot',
+    install_requires=['discord'],
+)
