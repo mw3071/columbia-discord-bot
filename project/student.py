01074261158f48f3da9adb245d921196cd5b25ca
mw3071 <103603425+mw3071@users.noreply.github.com>
2023-04-10 15:26:18 -0400

    added auto doc compatible doc strings  for part1

--- project/student.py
+++ project/student.py
@@ -5,9 +5,6 @@ class Student(object):
         self.classes = []
         self.profs = []
 
-    def _cmpkey(self):
-        return self.uni
-
     def __str__(self) -> str:
         ret = ""
         ret += self.username + ", " + self.uni + ", ["
@@ -21,9 +18,11 @@ class Student(object):
         return ret
 
     def set_uni(self, u):
+        '''Takes in a string u and changes the student's uni to u'''
         self.uni = u
 
     def add_class(self, c):
+        '''Takes in a string c and adds that to the student's classes array'''
         if c not in self.classes:
             self.classes.append(c)
             print("added!")
@@ -31,6 +30,7 @@ class Student(object):
             print("already added")
 
     def remove_class(self, c):
+        '''Takes in a string c and removes it from the student's classes array'''
         if c in self.classes:
             self.classes.remove(c)
             print("removed")
@@ -38,11 +38,13 @@ class Student(object):
             print("class not found")
 
     def add_prof(self, p):
+        '''Takes in a string c and adds that to the student's prof array'''
         if p not in self.profs:
             self.profs.append(p)
             print("added!")
 
     def remove_prof(self, p):
+        '''Takes in a string c and removes it from the student's prof array'''
         if p in self.profs:
             self.profs.remove(p)
             print("removed")
@@ -50,7 +52,9 @@ class Student(object):
             print("prof not found")
 
     def get_profs(self):
+        '''returns the student's prof array'''
         return self.profs
 
     def get_classes(self):
+        '''returns the student's classes array'''
         return self.classes
