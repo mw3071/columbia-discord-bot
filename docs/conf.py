a1cb2d2f020b8dfaf4c534d2d1861d5cab8e1f96
mw3071 <103603425+mw3071@users.noreply.github.com>
2023-05-04 21:24:19 -0400

    added sphinx documentation files

---
+++ docs/conf.py
@@ -0,0 +1,38 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'columbia-discord-bot'
+copyright = '2023, Matthew Wang'
+author = 'Matthew Wang'
+release = '0.1.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['recommonmark']
+source_suffix = ['.rst', '.md']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_static_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+from recommonmark.transform import AutoStructify
+
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+        'auto_toc_tree_section': 'Contents',
+    }, True)
+    app.add_transform(AutoStructify)    
---
+++ docs/index.md
@@ -0,0 +1,15 @@
+# Welcome to columbia-discord-bot's documentation!
+
+
+## This is a first header
+
+
+- These
+- Are
+- Some
+- Bullets
+
+```eval_rst
+.. note::
+      We can still mix rst, which we'll use for autodocumentation
+```
