46f01405553c1001cc0237b87622a11e4e28ec23
mw3071 <103603425+mw3071@users.noreply.github.com>
2023-05-07 19:50:11 -0400

    added theme

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ source_suffix = ['.rst', '.md']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
