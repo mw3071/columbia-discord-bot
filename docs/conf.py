cbf261be8e4e04fba4b29f28048fdd1fdc24ae24
mw3071 <103603425+mw3071@users.noreply.github.com>
2023-05-07 20:38:16 -0400

    installed sphinx napoleon for autodocumentation

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ release = '0.1.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['recommonmark']
+extensions = ['recommonmark', 'sphinxcontrib.napoleon']
 source_suffix = ['.rst', '.md']
 
 templates_path = ['_templates']
