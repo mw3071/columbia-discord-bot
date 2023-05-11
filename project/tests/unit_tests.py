5d314ed032fa3243a6dc350dd985cb2bf224c94a
mw3071 <103603425+mw3071@users.noreply.github.com>
2023-05-10 22:21:27 -0400

    added new lookup function

--- project/tests/unit_tests.py
+++ project/tests/unit_tests.py
@@ -6,7 +6,7 @@ def test_add_class():
     s = student.Student('test-username', 'test-uni')
     print('testing add class')
     s.add_class('test-class')
-    assert s.classes == ['test-class']
+    assert s.get_classes() == ['test-class']
     print('done')
 
 
@@ -14,7 +14,7 @@ def test_add_prof():
     s = student.Student('test-username', 'test-uni')
     print('testing add profs')
     s.add_prof('test-prof')
-    assert s.profs == ['test-prof']
+    assert s.get_profs() == ['test-prof']
     print('done')
 
 
@@ -23,7 +23,7 @@ def test_remove_class():
     print('testing remove class')
     s.add_class('test-class')
     s.remove_class('test-class')
-    assert s.classes == []
+    assert s.get_classes() == []
     print('done')
 
 
@@ -32,7 +32,7 @@ def test_remove_prof():
     print('testing remove profs')
     s.add_prof('test-prof')
     s.remove_prof('test-prof')
-    assert s.profs == []
+    assert s.get_profs() == []
     print('done')
 
 
@@ -53,3 +53,41 @@ def test_get_profs():
     s = student.Student('test-username', 'test-uni')
     s.add_prof('test-prof')
     assert s.get_profs() == ['test-prof']
+
+def test_look_up_class():
+    users = {}
+    s = student.Student('student1', 'uni1')
+    s1 = student.Student('student2', 'uni2')
+    s2 = student.Student('student3', 'uni3')
+
+    users['student1'] = s
+    users['student2'] = s1
+    users['student3'] = s2
+
+    s.add_class('test-class')
+    s1.add_class('test-class')
+    s2.add_class('test-class-1')
+
+    test_str = s1.look_up_class('test-class', users)
+    assert test_str == [s.uni, s1.uni]
+    test_str = s1.look_up_class('test-class-1', users)
+    assert test_str == [s2.uni]
+
+def test_look_up_prof():
+    users = {}
+    s = student.Student('student1', 'uni1')
+    s1 = student.Student('student2', 'uni2')
+    s2 = student.Student('student3', 'uni3')
+
+    users['student1'] = s
+    users['student2'] = s1
+    users['student3'] = s2
+
+    s.add_prof('test-prof')
+    s1.add_prof('test-prof')
+    s2.add_prof('test-prof-1')
+
+    test_str = s1.look_up_prof('test-prof', users)
+    assert test_str == [s.uni, s1.uni]
+    test_str = s1.look_up_prof('test-prof-1', users)
+    assert test_str == [s2.uni]
